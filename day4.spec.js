e4f122d41ccc550e1947ca0ad0f6a6662ba69d42
kephin <wunshon76325@gmail.com>
2019-12-22 23:51:53 +0800

    day4-2

--- day4.spec.js
+++ day4.spec.js
@@ -28,6 +28,21 @@ const isAdjacentTheSame = digit => {
   return false
 }
 
+const isTwoAdjacentNotPartOfALargerGroup = digit => {
+  const digitStr = digit.toString()
+  const digitArray = []
+  for (let index = 0; index < 6; index++) {
+    digitArray.push(digitStr[index])
+    if (digitStr[index] !== digitStr[index + 1]) {
+      digitArray.push('|')
+    }
+  }
+  for (const part of digitArray.join('').split('|')) {
+    if (part.length === 2) return true
+  }
+  return false // no adjacent matching digits
+}
+
 test('day4-1', t => {
   let counter = 0
   for (let digit = 152085; digit <= 670283;) {
@@ -45,4 +60,21 @@ test('day4-1', t => {
   }
 
   t.is(counter, 1764)
+})
+
+test('day4-2', t => {
+  let counter = 0
+  for (let digit = 152085; digit <= 670283;) {
+    const [isNeverDecrease, newDigit] = calculateIsNeverDecrease(digit)
+
+    if (!isNeverDecrease) {
+      digit = newDigit
+      continue
+    }
+
+    // new rule
+    if (isTwoAdjacentNotPartOfALargerGroup(digit)) counter++
+    digit++
+  }
+  t.is(counter, 1196)
 })
